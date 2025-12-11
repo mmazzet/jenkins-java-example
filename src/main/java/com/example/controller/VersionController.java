415f735b90bc7822237dc151377577b81625d66f
Marianna Mazzette <20104120@mail.wit.ie>
2025-12-11 18:37:43 +0000

    initial commit

---
+++ src/main/java/com/example/controller/VersionController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.HashMap;
+
+@RestController
+public class VersionController {
+
+  @Value("${app.version}")
+  private String version;
+
+  @Value("${build.timestamp}")
+  private String buildTimestamp;
+
+  @GetMapping("/version")
+  public Map<String, String> getVersion() {
+    Map<String, String> map = new HashMap<>();
+    map.put("version", version);
+    map.put("buildTimestamp", buildTimestamp);
+    return map;
+  }
+}
