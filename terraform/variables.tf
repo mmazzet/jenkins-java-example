7d6e6a3992cb31aba75accdbbe8bfeb8aa06131d
Marianna Mazzette <20104120@mail.wit.ie>
2026-02-22 18:02:51 +0000

    fix subnet cidr

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@ variable vpc_cidr_block {
     default = "10.0.0.0/16"
 }
 variable subnet_cidr_block {
-    default = "10.0.10.0/16"
+    default = "10.0.10.0/24"
 }
 variable avail_zone {
     default = "eu-west-1a"
